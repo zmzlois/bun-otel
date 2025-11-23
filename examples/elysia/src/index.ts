ece1248a78506a736d212bcc26fcda883b48068c
zmzlois <lois.z@comcord.vision>
2025-11-23 18:48:44 +0000

    add elysia test app

---
+++ examples/elysia/src/index.ts
@@ -0,0 +1,12 @@
+import { Elysia } from "elysia";
+import { OpenTelemetryClient } from "bun-otel";
+
+const client = new OpenTelemetryClient();
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
+
+console.log("something");
